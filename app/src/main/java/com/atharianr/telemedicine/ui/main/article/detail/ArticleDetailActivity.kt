439c2f0c17e1ec0b11a4f46f749a6dde62486bea
atharianr <atharianrahmadani@gmail.com>
2022-04-26 01:49:49 +0700

    changing article detail background theme

--- app/src/main/java/com/atharianr/telemedicine/ui/main/article/detail/ArticleDetailActivity.kt
+++ app/src/main/java/com/atharianr/telemedicine/ui/main/article/detail/ArticleDetailActivity.kt
@@ -3,10 +3,14 @@ package com.atharianr.telemedicine.ui.main.article.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.atharianr.telemedicine.R
+import com.atharianr.telemedicine.databinding.ActivityArticleDetailBinding
+import com.atharianr.telemedicine.databinding.ActivityChatBinding
 
 class ArticleDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_article_detail)
+        val binding = ActivityArticleDetailBinding.inflate(layoutInflater)
+        setTheme(R.style.Theme_Telemedicine)
+        setContentView(binding.root)
     }
 }
