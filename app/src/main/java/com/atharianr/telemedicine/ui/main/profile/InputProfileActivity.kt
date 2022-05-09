b7a150e1277d9bf4332d4eaf8ae1fcb80c119583
atharianr <atharianrahmadani@gmail.com>
2022-05-09 09:02:59 +0700

    input profile

--- app/src/main/java/com/atharianr/telemedicine/ui/main/profile/InputProfileActivity.kt
+++ app/src/main/java/com/atharianr/telemedicine/ui/main/profile/InputProfileActivity.kt
@@ -2,10 +2,12 @@ package com.atharianr.telemedicine.ui.main.profile
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.atharianr.telemedicine.R
 import com.atharianr.telemedicine.databinding.ActivityInputProfileBinding
 import com.atharianr.telemedicine.ui.main.MainActivity
+
 
 class InputProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +17,19 @@ class InputProfileActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.apply {
+            val gender = arrayOf("Laki-laki", "Perempuan")
+            val genderArrayAdapter =
+                ArrayAdapter(this@InputProfileActivity, R.layout.spinner, gender)
+            spinnerGender.adapter = genderArrayAdapter
+
+            val blood = arrayOf("A", "B", "AB", "O")
+            val bloodArrayAdapter = ArrayAdapter(this@InputProfileActivity, R.layout.spinner, blood)
+            spinnerBlood.adapter = bloodArrayAdapter
+
             btnSave.setOnClickListener {
                 val intent = Intent(this@InputProfileActivity, MainActivity::class.java)
                 startActivity(intent)
+                finish()
             }
         }
     }
