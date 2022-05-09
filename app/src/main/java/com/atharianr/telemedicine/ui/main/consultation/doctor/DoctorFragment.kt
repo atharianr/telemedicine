64c94a55b4d307ffd9f41dc0988dc50bb239505f
atharianr <atharianrahmadani@gmail.com>
2022-05-09 09:43:18 +0700

    adding dialog filter

--- app/src/main/java/com/atharianr/telemedicine/ui/main/consultation/doctor/DoctorFragment.kt
+++ app/src/main/java/com/atharianr/telemedicine/ui/main/consultation/doctor/DoctorFragment.kt
@@ -1,18 +1,23 @@
 package com.atharianr.telemedicine.ui.main.consultation.doctor
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.atharianr.telemedicine.R
+import com.atharianr.telemedicine.databinding.DialogFilterBinding
 import com.atharianr.telemedicine.databinding.FragmentDoctorBinding
 import com.atharianr.telemedicine.utils.DummyData
 
 class DoctorFragment : Fragment() {
 
     private var _binding: FragmentDoctorBinding? = null
+    private var _dialogBinding: DialogFilterBinding? = null
     private val binding get() = _binding as FragmentDoctorBinding
+    private val dialogBinding get() = _dialogBinding as DialogFilterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +25,7 @@ class DoctorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDoctorBinding.inflate(inflater, container, false)
+        _dialogBinding = DialogFilterBinding.inflate(layoutInflater)
         return binding.root
     }
 
@@ -27,13 +33,37 @@ class DoctorFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         if (activity != null) {
+            val dialog = Dialog(requireActivity())
+            dialog.setContentView(dialogBinding.root)
+            dialog.window?.setBackgroundDrawableResource(R.drawable.rounded_box_16)
+
             val doctorAdapter = DoctorAdapter()
             doctorAdapter.setData(DummyData.getDoctor())
 
-            binding.rvDoctor.apply {
-                layoutManager = LinearLayoutManager(context)
-                setHasFixedSize(true)
-                adapter = doctorAdapter
+            binding.apply {
+                btnFilter.setOnClickListener {
+                    dialog.show()
+                }
+
+                rvDoctor.apply {
+                    layoutManager = LinearLayoutManager(context)
+                    setHasFixedSize(true)
+                    adapter = doctorAdapter
+                }
+            }
+
+            dialogBinding.apply {
+                btnCancel.setOnClickListener {
+                    dialog.dismiss()
+                }
+
+                btnUse.setOnClickListener {
+                    dialog.dismiss()
+                }
+
+                btnDelete.setOnClickListener {
+                    dialog.dismiss()
+                }
             }
         }
     }
